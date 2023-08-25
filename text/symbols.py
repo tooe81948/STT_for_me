30a4c20a4b3f8b96f6a116c2c3986da9c8b015dd
Suea_Ep_ET <75261270+tooe81948@users.noreply.github.com>
2023-08-25 18:18:56 +0700

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -4,7 +4,7 @@ Defines the set of symbols used in text input to the model.
 
 _pad        = '_'
 _punctuation = ',.!?-~…'
-_letters = 'AEINOQUabdefghijklmnoprstuvwyzʃʧʦɯɹəɥ⁼ʰ`→↓↑ '
+_letters = 'AEINOQUabdefghijklmnoprstuvwyzʃʧʦɯɹəɥ⁼ʰ`→↓↑ ๆไำพะัีรนยบลฟหกดเ้่าสวงผปแอิืทมใฝฦฬฒ?์ฺฮฉ.ซศษ๋็ฌโฏฆฤ๐"ฎฑธํ๊ณฯญฐภถุูึคตจขช๙๘๗๖๕๔๓๒๑)'
 
 
 
